a8f7615911499e78aea1cb6be765223a57a4f2ea
Xueyan <xueyan@gulory.com>
2012-02-11 14:17:58 -0500

    Git test for commit/push.

---
+++ NewTest/src/com/xueyan/git/test/HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.xueyan.git.test;
+
+/**
+ * @author xz2248
+ *
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello world.");
+	}
+}
